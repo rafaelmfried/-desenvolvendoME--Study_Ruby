fdb8ef3c6926d2b415559187aeaee05237fb4b34
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-04-25 20:38:26 -0300

    feat: exercices 12 - 13

---
+++ learn-ruby-the-hard-way-course/exercice13.rb
@@ -0,0 +1,12 @@
+# Parameters, Unpacking, Variables
+
+
+first, second, third = ARGV
+
+variable = $stdin.gets.chomp
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is #{third}"
+
+puts "Yours stdin input is #{variable}."
