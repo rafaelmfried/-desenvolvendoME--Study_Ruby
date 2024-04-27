b924b6880f711d55f7a769580ba560f4b440db1a
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-04-27 09:45:45 -0300

    feat: exercice 36 - Understanding important rules about use of the if, else, for and while statements.

---
+++ learn-ruby-the-hard-way-course/exercice36.rb
@@ -0,0 +1,13 @@
+# Designing and Debugging
+
+# Every if must have an else
+
+# Never nest if-statements more than two deep and always try to do them one deep.
+
+# Trat if-statement like paragraphs, where each if-elsif-else grouping is like a set of sentences. Put blank lines before and after
+
+# Use good name for the variables.
+
+# Use while loop only for loop forever
+
+# Use for-loop for all other kinds of looping, especiallu if there is a fixed or limited number of things to loop over.
