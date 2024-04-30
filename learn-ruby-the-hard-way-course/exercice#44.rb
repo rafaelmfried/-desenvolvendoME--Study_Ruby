381a2ad23501843f73b27bf545337ebb157568a8
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-04-30 12:15:03 -0300

    feat: exercice 44 - Learning about composition and mixin in ruby poo

--- learn-ruby-the-hard-way-course/exercice#44.rb
+++ learn-ruby-the-hard-way-course/exercice#44.rb
@@ -2,7 +2,7 @@
 
 # Implicit Inheritance - When u don't declare the safe method in the Child the Child will inherit the method of the Parent.
 
-puts "IMPLICIT> "
+puts "\nIMPLICIT> \n"
 
 class Parent
     def implicit()
@@ -21,7 +21,7 @@ son.implicit()
 
 # Override explicity - When u declare a method in a Child that exists in the Parent this method will be override in the Child.
 
-puts "OVERRIDE> "
+puts "\nOVERRIDE> \n"
 
 class ParentOverride
     def override()
@@ -43,7 +43,7 @@ son.override()
 
 # Alter Before or After - With the super we can access the method of the Parent inside the override method in the Child.
 
-puts "SUPER> "
+puts "\nSUPER> \n"
 
 class ParentSuper
     def altered()
@@ -67,7 +67,7 @@ son.altered()
 
 # All Three Combined
 
-puts "ALL_METHODS> "
+puts "\nALL_METHODS> \n"
 
 class ParentAll
     def override()
@@ -112,9 +112,96 @@ son.altered()
 
 # Using super() with initialize
 
+puts "\nINITIALIZE> \n"
+
 class Child < Parent
     def initialize(stuff)
         @stuff = stuff
+        puts "#{stuff}"
         super()
     end
 end
+
+son = Child.new("Ola mundo")
+
+# Composition - This is the other way works looks like a inheritance  * [has-a relationship]
+
+puts "\nCOMPOSITION> \n"
+
+class Other
+    def override()
+        puts "OTHER override()"
+    end
+
+    def implicit()
+        puts "OTHER implicit()"
+    end
+
+    def altered()
+        puts "OTHER altered()"
+    end
+end
+
+class ChildComposition
+    def initialize()
+        @other = Other.new()
+    end
+
+    def implicit()
+        @other.implicit()
+    end
+
+    def override()
+        puts "CHILD override()"
+    end
+
+    def altered()
+        puts "BEFORE CHILD altered()"
+        @other.altered()
+        puts "AFTER CHILD altered()"
+    end
+end
+
+son = ChildComposition.new()
+
+son.implicit()
+son.override()
+son.altered()
+
+# Composition with mixins - Mixins is a much more powerfull concept and advanced topic.
+
+puts "\nCOMPOSITION WITH MIXINS\n"
+
+module OtherMixins
+    def override()
+        puts "OTHER override()"
+    end
+
+    def implicit()
+        puts "OTHER implicit()"
+    end
+
+    def Other.altered()
+        puts "OTHER altered()"
+    end
+end
+
+class ChildMixins
+    include OtherMixins
+
+    def override()
+        puts "CHILD override()"
+    end
+
+    def altered()
+        puts "BEFORE CHILD altered()"
+        Other.altered()
+        puts "AFTER CHILD altered()"
+    end
+end
+
+son = ChildMixins.new()
+
+son.implicit()
+son.override()
+son.altered()
