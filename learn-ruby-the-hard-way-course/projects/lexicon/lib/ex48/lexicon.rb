c05fbf4b056941bd1bfa0ae81ab8c4407625b3f7
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-05 20:58:18 -0300

    feat: implement the Lexicon class based in lexicon test

---
+++ learn-ruby-the-hard-way-course/projects/lexicon/lib/ex48/lexicon.rb
@@ -0,0 +1,24 @@
+class Lexicon
+
+  LEXICON = {
+    'direction' =>  ['north', 'south', 'east'],
+    'verb' => ['go', 'kill', 'eat']
+  }
+
+  def self.scan(sentence)
+    words = sentence.split
+    result = []
+    words.each do |word|
+      category = category_indetifier(word)
+      result << [category, word]
+    end
+    result
+  end
+
+  def self.category_indetifier(word)
+    LEXICON.each do |key, values|
+      return key if values.include?(word.downcase)
+    end
+    'error'
+  end
+end
