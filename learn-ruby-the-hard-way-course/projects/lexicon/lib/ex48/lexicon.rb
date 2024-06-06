9c470e613c2aee68a880cd33c78f5ec3986158a3
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-05 21:35:56 -0300

    feat: number lexicon

--- learn-ruby-the-hard-way-course/projects/lexicon/lib/ex48/lexicon.rb
+++ learn-ruby-the-hard-way-course/projects/lexicon/lib/ex48/lexicon.rb
@@ -4,20 +4,23 @@ class Lexicon
     'direction' =>  ['north', 'south', 'east'],
     'verb' => ['go', 'kill', 'eat'],
     'stop' => ['the', 'in', 'of'],
-    'noun' => ['bear', 'princess', 'castle']
+    'noun' => ['bear', 'princess', 'castle'],
   }
 
   def self.scan(sentence)
     words = sentence.split
     result = []
     words.each do |word|
+      
       category = category_indetifier(word)
+      word = category == 'number' ? word.to_i : word
       result << [category, word]
     end
     result
   end
 
   def self.category_indetifier(word)
+    return 'number' if Integer(word) rescue
     LEXICON.each do |key, values|
       return key if values.include?(word.downcase)
     end
